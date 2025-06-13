cc21b11614c3028ff0d7cb8a680797cd4169e9aa
Dany fatihul <fatihuldanyy@gmail.com>
2025-06-13 15:10:45 +0700

    Add Aquanest/admin/includes/report_empty.php 1749802246026

---
+++ Aquanest/admin/includes/report_empty.php
@@ -0,0 +1,9 @@
+<div class="alert alert-info">
+    <div class="d-flex align-items-center">
+        <i class="fas fa-info-circle fa-2x me-3"></i>
+        <div>
+            <h5 class="mb-1">Belum Ada Data</h5>
+            <p class="mb-0">Pilih jenis laporan dan rentang tanggal untuk melihat data.</p>
+        </div>
+    </div>
+</div>
