ffa8608620a40dbd09177d4d71678c68ba537879
Dany fatihul <fatihuldanyy@gmail.com>
2025-06-13 15:11:07 +0700

    Add Aquanest/admin/logout.php 1749802268965

---
+++ Aquanest/admin/logout.php
@@ -0,0 +1,33 @@
+<?php
+// Start session
+session_start();
+
+// Include necessary files
+require_once '../includes/auth.php';
+require_once '../includes/functions.php';
+
+// Start session
+session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Regenerate session ID for security reasons in case of session fixation attempts
+session_regenerate_id(true);
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the home page (index.php) instead of login page
+header("location: login.php");
+exit;
+?>
