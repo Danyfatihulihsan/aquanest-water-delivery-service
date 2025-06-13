3f84fa809083c6605d6b3ceff1f2a4a4926d48f8
Dany fatihul <fatihuldanyy@gmail.com>
2025-06-13 15:09:58 +0700

    Add Aquanest/includes/mail-config.php 1749802199103

---
+++ Aquanest/includes/mail-config.php
@@ -0,0 +1,12 @@
+<?php
+// Konfigurasi Email untuk Gmail
+return [
+    'host' => 'smtp.gmail.com',
+    'username' => 'fatihuldanyy@gmail.com', // Ganti dengan email Gmail Anda
+    'password' => 'wbye qguz eleo rdea', // Gunakan App Password Gmail (bukan password biasa)
+    'port' => 587,
+    'encryption' => 'tls',
+    'from_email' => 'fatihuldanyy@gmail.com', // Ganti dengan email Gmail Anda
+    'from_name' => 'Aquanest Support'
+];
+?>
