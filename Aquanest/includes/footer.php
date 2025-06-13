6a2dc6c9f4e13f22293411da40878e71189645d0
Dany fatihul <fatihuldanyy@gmail.com>
2025-06-13 15:10:04 +0700

    Add Aquanest/includes/footer.php 1749802205116

---
+++ Aquanest/includes/footer.php
@@ -0,0 +1,54 @@
+<footer class="bg-dark text-white py-5 ">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4 mb-md-0">
+                <h5 class="mb-3">Aquanest</h5>
+                <p>Depot air minum berkualitas, menyediakan air murni dengan proses pengolahan modern untuk memastikan kesehatan Anda dan keluarga.</p>
+                <div class="d-flex mt-3">
+                    <a href="#" class="text-white me-3"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="text-white me-3"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="text-white me-3"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="text-white"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+            <div class="col-md-2 mb-4 mb-md-0">
+                <h5 class="mb-3">Navigasi</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="index.php" class="text-white text-decoration-none">Beranda</a></li>
+                    <li class="mb-2"><a href="products.php" class="text-white text-decoration-none">Produk</a></li>
+                    <li class="mb-2"><a href="order.php" class="text-white text-decoration-none">Pesan</a></li>
+                    <li class="mb-2"><a href="about.php" class="text-white text-decoration-none">Tentang Kami</a></li>
+                    <li><a href="contact.php" class="text-white text-decoration-none">Kontak</a></li>
+                   
+                </ul>
+            </div>
+            <div class="col-md-3 mb-4 mb-md-0">
+                <h5 class="mb-3">Produk Kami</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="products.php" class="text-white text-decoration-none">Air Galon</a></li>
+                    <li class="mb-2"><a href="products.php" class="text-white text-decoration-none">Air isi ulang </a></li>
+                    <li class="mb-2"><a href="products.php" class="text-white text-decoration-none">Air Botol</a></li>
+                    <li class="mb-2"><a href="products.php" class="text-white text-decoration-none">Air Gelas</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h5 class="mb-3">Hubungi Kami</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><i class="fas fa-map-marker-alt me-2"></i> Jl. Griya Family 4 No.17, Sarimukti, Kec. Cibitung, Kabupaten Bekasi, Jawa Barat 17520</li>
+                    <li class="mb-2"><i class="fas fa-phone-alt me-2"></i> (0818) 0607-9131</li>
+                    <li class="mb-2"><i class="fas fa-mobile-alt me-2"></i> 0895-3000-7056</li>
+                    <li><i class="fas fa-envelope me-2"></i> info@aquanest.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-4">
+        <div class="row">
+            <div class="col-md-6 text-center text-md-start">
+                <p class="mb-md-0">&copy; <?php echo date('Y'); ?> Aquanest. Semua hak dilindungi.</p>
+            </div>
+            <div class="col-md-6 text-center text-md-end">
+                <p class="mb-0">Jam Operasional: Senin - Sabtu, 08.00 - 17.00 WIB</p>
+            </div>
+        </div>
+    </div>
+</footer>
